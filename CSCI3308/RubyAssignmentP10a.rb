2f3c810c3b57b7b283400b82a2e780200e460fea
Sheefali Tewari <sheefali.tewari@colorado.edu>
2014-02-05 01:19:03 -0700

    inital commit

--- CSCI3308/RubyAssignmentP10a.rb
+++
@@ -1,8 +0,0 @@
-#Part 10a: Palindromes
-
-class String
-	def palindrome?
-		temp_str = self.downcase.gsub(/\W/, "")
-		temp_str == temp_str.reverse 
-	end
-end
--- CSCI3308/RubyAssignmentP10b.rb
+++
@@ -1,7 +0,0 @@
-#Part 10b: Palindromes again
-module Enumerable
-	def palindrome?
-		array = [] #=> make empty array
-		self.collect{|num| num} == self.collect{|num|num}.reverse #=> compare array of numbers to its reversed self
-	end
-end
